8145904ca0b9fc2ea4dec6ed1c518401f766fed7
gareth <gareth@nofeveryone.com>
2015-04-27 12:55:03 -0500

    json
    json

--- doi.js
+++ doi.js
@@ -4,10 +4,10 @@ console.log("heyyyy " + doi);
 
 var rep = doi.replace("?","");
 $.ajax({
-      url: "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?retmode=xml&db=pmc&term="+doi+"[doi]",
+      url: "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?retmode=json&db=pmc&term="+doi+"[doi]",
       datatype: "json",
       success: function(data) {
         console.log(data);
-        
+
       }
 });
