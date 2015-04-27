f54e7afd72c58c839165104668728907fa410c1b
gareth <gareth@nofeveryone.com>
2015-04-27 13:09:23 -0500

    runnin

--- doi.js
+++ doi.js
@@ -3,11 +3,26 @@ var doi = window.location.search;
 console.log("heyyyy " + doi);
 
 var rep = doi.replace("?","");
-$.ajax({
-      url: "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?retmode=json&db=pmc&term="+doi+"[doi]",
-      datatype: "json",
-      success: function(data) {
-        console.log(data);
 
-      }
+var storeId;
+var idFind = function (doi) {
+  $.ajax({
+    url: "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?retmode=json&db=pmc&term="+doi+"[doi]",
+    datatype: "json",
+    success: function(data) {
+      storeId = data.esearchresult.idlist[0];
+      console.log(storeId);
+    }
+  });
+}
+
+$.when(idFind(doi)).done( function (storeId) {
+  console.log('part deux');
+  $.ajax({
+    url: "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?retmode=xml&db=pmc&id="+storeId,
+    datatype: "xml",
+    success: function(data) {
+      console.log(data);
+    }
+  });
 });
