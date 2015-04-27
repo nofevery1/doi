c62a3db3b2d8805f955d22b8342b7bd8533c2d9e
gareth <gareth@nofeveryone.com>
2015-04-27 12:41:40 -0500

    ajax 1

--- doi.js
+++ doi.js
@@ -1,3 +1,12 @@
 var doi = window.location.search;
 
 console.log("heyyyy " + doi);
+
+var rep = doi.replace("?","");
+$.ajax({
+      url: "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pmc&retmode=json&term="+doi,
+      datatype: "json",
+      success: function(data) {
+        console.loge(data);
+      }
+});
